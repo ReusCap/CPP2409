1b67e6b2ecd637d9953a5561f91eed5bb1d5ff94
ReusCap <nick921@naver.com>
2024-09-09 15:28:37 +0900

    Upload First Program

---
+++ practice/week2/first_program.cpp
@@ -0,0 +1,12 @@
+// 첫 번쨰 예제 프로그램
+// ctrl + B 하면 왼쪽 파일창 열기
+// ctrl +나 -하면 크기조절
+// ctrl +s하면 저장
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
